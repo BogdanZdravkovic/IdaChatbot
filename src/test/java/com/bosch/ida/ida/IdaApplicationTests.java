c39a00d76960aa9412b9825699ca26e562079cf1
EMEA\ZDB1BG <Bogdan.Zdravkovic@bosch.com>
2018-12-17 16:11:12 +0100

    initial commit

---
+++ src/test/java/com/bosch/ida/ida/IdaApplicationTests.java
@@ -0,0 +1,17 @@
+package com.bosch.ida.ida;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IdaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
